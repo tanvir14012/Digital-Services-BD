a375b123c2b215fa0691f92d64cda202cfbc844e
Tanvir Ahmed <tanvir14012@gmail.com>
2020-06-14 02:23:50 +0600

    Daily progress
    Product category filter by price, sortby, category links, pagination

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Digital_Services_BD.Models;
+using Digital_Services_BD.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Digital_Services_BD.Controllers
+{
+    [Route("[controller]/{id}")]
+    public class CategoryController : Controller
+    {
+        private readonly IProductGroupOps productGroupOps;
+        private readonly IProductCategoryOps productCategoryOps;
+
+        public CategoryController(IProductGroupOps productGroupOps, IProductCategoryOps productCategoryOps)
+        {
+            this.productGroupOps = productGroupOps;
+            this.productCategoryOps = productCategoryOps;
+        }
+        public IActionResult Index([FromRoute] int id, [FromQuery] int pageNo = 1,
+            [FromQuery] string SortBy = null, [FromQuery] string priceRange = null)
+        {
+            if(ModelState.IsValid)
+            {
+                var productGroup = productGroupOps.GetProductGroup(id);
+                if (productGroup != null)
+                {
+                    ViewBag.Id = productGroup.Id;
+                    ViewBag.ProductGroupName = productGroup.Name;
+                    ViewBag.TotalCategory = productGroupOps.GetAllProdCategoriesByProdGroupId(id).Count();
+                    ViewBag.PageNo = pageNo;
+                    return View(productGroup.AllCategories.ToList());
+                }
+            }
+      
+            return View("NotFound");
+        }
+    }
+}
